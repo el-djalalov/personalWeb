26e492d2694dde1330fb1ab958c5213b18a904e5
Elyor Djalalov <edjalalov@hotmail.com>
2024-10-12 13:04:33 +0500

    updates to deploy on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,16 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://elyor.dev',
-  //output: 'static',
-  base: '/',
+  output: 'static',
+  /* base: '/',
   build: {
     format: 'directory'
   },
   server: {
     host: true
-  }
+  } */
 });

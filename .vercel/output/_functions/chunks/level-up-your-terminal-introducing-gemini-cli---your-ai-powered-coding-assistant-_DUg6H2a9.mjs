d5d9a12b01a325cb4db48985eb7e5314ae016b0b
E.Djalalov <edjalalov@hotmail.com>
2026-01-30 19:32:14 +0500

    updates

--- .vercel/output/_functions/chunks/level-up-your-terminal-introducing-gemini-cli---your-ai-powered-coding-assistant-_DUg6H2a9.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./level-up-your-terminal-introducing-gemini-cli---your-ai-powered-coding-assistant-_qHbC2eKY.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };

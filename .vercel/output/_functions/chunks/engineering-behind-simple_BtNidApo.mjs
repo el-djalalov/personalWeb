5f3d51ee61036d9111eac62aba31bccafd85b88a
E.Djalalov <edjalalov@hotmail.com>
2026-01-30 20:26:27 +0500

    removed Tina

--- .vercel/output/_functions/chunks/engineering-behind-simple_BtNidApo.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./engineering-behind-simple_q5Tl9q_D.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
